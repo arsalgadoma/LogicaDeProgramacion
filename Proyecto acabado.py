2141b48415352c965b1602a6dac50d3e2a5b4eda
arsalgadoma <arsalgadoma@uide.edu.ec>
2024-11-27 22:08:24 -0500

    Update Proyecto acabado.py

--- Proyecto acabado.py
+++ Proyecto acabado.py
@@ -11,12 +11,12 @@ while True:
   print("4. Salir")
 
   #Solicitar opción de usuario
-  OP_JUGADDOR = (input("Selecciona una opción (1-3): "))
+  OP_JUGADOR = (input("Selecciona una opción (1-3): "))
 
   #Eleccion de computadora
   OP_COMP=choice(["1", "2", "3"])
 
-  if  OP_JUGADDOR=="1"or OP_JUGADDOR=="2" or OP_JUGADDOR=="3":
+  if  OP_JUGADOR=="1" or OP_JUGADOR=="2" or OP_JUGADOR=="3":
     #Mostrar eleccion del computadora
     if  OP_COMP=="1":
       print ("La opcion de la computadora es: Piedra")
@@ -26,29 +26,29 @@ while True:
       print ("La opcion de la computadora es: Tijera")
 
     #Mostrar eleccion del jugador
-    if  OP_JUGADDOR=="1":
+    if  OP_JUGADOR=="1":
       print ("Tu elección fue: Piedra")
-    elif OP_JUGADDOR=="2":
+    elif OP_JUGADOR=="2":
       print ("Tu elección fue: Papel")
-    elif OP_JUGADDOR=="3":
+    elif OP_JUGADOR=="3":
       print ("Tu elección fue: Tijera")
 
     #Mostrar resultado
 
-    if  OP_JUGADDOR==OP_COMP:
+    if  OP_JUGADOR==OP_COMP:
       print ("\n\t>>> EMPATE <<<")
-    elif (OP_JUGADDOR=="1" and OP_COMP=="3") or (OP_JUGADDOR=="2" and OP_COMP=="1") or (OP_JUGADDOR=="3" and OP_COMP=="2"):
+    elif (OP_JUGADOR=="1" and OP_COMP=="3") or (OP_JUGADOR=="2" and OP_COMP=="1") or (OP_JUGADOR=="3" and OP_COMP=="2"):
       print ("\n\t>>> GANASTE <<<")
-    elif (OP_JUGADDOR=="1" and OP_COMP=="2") or (OP_JUGADDOR=="2" and OP_COMP=="3") or (OP_JUGADDOR=="3" and OP_COMP=="1"):
+    elif (OP_JUGADOR=="1" and OP_COMP=="2") or (OP_JUGADOR=="2" and OP_COMP=="3") or (OP_JUGADOR=="3" and OP_COMP=="1"):
       print ("\n\t>>> PERDISTE <<<")    
-    
+
     #Preguntar si desea continuar con el juego
-    respuesta = input("\n¿Quieres seguir jugando? (s/n): ").lower()
-    if respuesta != "s" and respuesta != "Si" and respuesta != "si":
+    respuesta = input("\n¿Quieres seguir jugando? (s/n): ")
+    if respuesta != "s" and respuesta != "Si" and respuesta != "si" and respuesta != "S" and respuesta != "SI":
         print("Gracias por participar")
         break # Salir del bucle y terminar el programa
-  
-  elif OP_JUGADDOR=="4":
+
+  elif OP_JUGADOR=="4":
     print ("Gracias por jugar")
     break # Salir del bucle y terminar el programa
   else:
