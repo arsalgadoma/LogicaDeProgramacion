2293dae4be72f913fc6bac051d52c618b461ed27
arsalgadoma <arsalgadoma@uide.edu.ec>
2024-12-08 18:42:34 -0500

    Update Proyecto acabado.py

--- Proyecto acabado.py
+++ Proyecto acabado.py
@@ -10,43 +10,46 @@ while True:
   print("4. Salir")
 
   #Solicitar opción de usuario
-  OP_JUGADDOR = int(input("Selecciona una opción (1-3): "))
+  OP_JUGADDOR = input("Selecciona una opción (1-3): ")
 
-  #Eleccion de computadora
-  OP_COMP=randint(1, 3)
+  if  OP_JUGADDOR == "1" or OP_JUGADDOR == "2" or OP_JUGADDOR == "3":
+    #Eleccion de computadora
+    OP_COMP = str(print(choice(["1", "2", "3"])))
 
-  if  OP_JUGADDOR == 1 or OP_JUGADDOR == 2 or OP_JUGADDOR == 3:
-    #Mostrar eleccion de la computadora
-    if  OP_COMP == 1:
+    print ("Clase 2:",type(OP_COMP))
+
+    #Mostrar eleccion del computadora
+    if  OP_COMP == "1":
       print ("La opcion de la computadora es: Piedra")
-    elif OP_COMP == 2:
+    elif OP_COMP == "2":
       print ("La opcion de la computadora es: Papel")
     else:
       print ("La opcion de la computadora es: Tijera")
 
     #Mostrar eleccion del jugador
-    if  OP_JUGADDOR == 1:
+    if  OP_JUGADDOR == "1":
       print ("Tu elección fue: Piedra")
-    elif OP_JUGADDOR == 2:
+    elif OP_JUGADDOR == "2":
       print ("Tu elección fue: Papel")
-    elif OP_JUGADDOR == 3:
+    else:
       print ("Tu elección fue: Tijera")
 
-    #Mostrar resultado del juego
+    #Mostrar resultado
+
     if  OP_JUGADDOR == OP_COMP:
       print ("\n\t>>> EMPATE <<<")
-    elif (OP_JUGADDOR == 1 and OP_COMP == 3) or (OP_JUGADDOR == 2 and OP_COMP == 1) or (OP_JUGADDOR == 3 and OP_COMP == 2):
+    elif (OP_JUGADDOR == "1" and OP_COMP == "3") or (OP_JUGADDOR == "2" and OP_COMP == "1") or (OP_JUGADDOR == "3" and OP_COMP == "2"):
       print ("\n\t>>> GANASTE <<<")
-    elif (OP_JUGADDOR == 1 and OP_COMP == 2) or (OP_JUGADDOR == 2 and OP_COMP == 3) or (OP_JUGADDOR == 3 and OP_COMP == 1):
-      print ("\n\t>>> PERDISTE <<<")    
+    elif (OP_JUGADDOR == "1" and OP_COMP == "2") or (OP_JUGADDOR == "2" and OP_COMP == "3") or (OP_JUGADDOR == "3" and OP_COMP == "1"):
+      print ("\n\t>>> PERDISTE <<<") 
     
-    #Preguntar si desea continuar con el juego
+    """#Preguntar si desea continuar con el juego
     respuesta = input("\n¿Quieres seguir jugando? (s/n): ").lower()
     if respuesta != "s" and respuesta != "Si" and respuesta != "si":
         print("Saliendo del programa...")
-        break # Salir del bucle y terminar el programa
+        break # Salir del bucle y terminar el programa"""
   
-  elif OP_JUGADDOR == 4:
+  elif OP_JUGADDOR == "4":
     print ("Gracias por jugar")
     break # Salir del bucle y terminar el programa
   else:
