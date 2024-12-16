6ec46297d784e9cbcc67acf5fcb56008b8f8bc89
arsalgadoma <arsalgadoma@uide.edu.ec>
2024-12-15 19:37:27 -0500

    pro

--- prueba.py
+++ prueba.py
@@ -1,42 +1,53 @@
-#Declarar random, para eleccion aleatoria de números
+#Declarar random, para eleccion aleatoria de numeros
 from random import *
-#  Mensaje de Bienvenida
-print("Bienvenido a tu juevo de Pierda, Papel o Tijeras")
 
-#Elejir una opción de usuario
-print("1. Piedra")
-print("2. Papel")
-print("3. Tijera")
-OP_JUGADDOR = int(input("Elige una opción: "))
+while True:
+  #  Mensaje de Bienvenida
+  print("\n***Bienvenido a tu juevo de Pierda, Papel o Tijeras***")
+  print("1. Piedra")
+  print("2. Papel")
+  print("3. Tijera")
+  print("4. Salir")
 
-#Eleccion de computadora
-OP_COMP=int(1)
-print ("La opcion de la computadora es:", OP_COMP)
+  #Solicitar opción de usuario
+  OP_JUGADDOR = input("Selecciona una opción (1-3): ")
 
-#Mostrar elección de jugador
-if  OP_JUGADDOR == 1:
-    print ("Tu elección fue: Piedra")
-elif OP_JUGADDOR == 2:
-    print ("Tu elección fue: Papel")
-elif OP_JUGADDOR == 3:
-     print ("Tu elección fue: Tijera")
-else:
-    print ("Escogiste una opcion incorrecta")
+  if  OP_JUGADDOR == "1" or OP_JUGADDOR == "2" or OP_JUGADDOR == "3":
+    #Eleccion de computadora
+    OP_COMP = choice(["1", "2", "3"])
 
-#Mostrar opción de computador
-if  OP_COMP == 1:
-    print ("La opcion de la computadora: Piedra")
-elif OP_COMP == 2:
-    print ("La opcion de la computadora: Papel")
-else:
-    print ("La opcion de la computadora: Tijera")
+    #Mostrar eleccion del computadora
+    if  OP_COMP == "1":
+      print ("La opcion de la computadora es: Piedra")
+    elif OP_COMP == "2":
+      print ("La opcion de la computadora es: Papel")
+    else:
+      print ("La opcion de la computadora es: Tijera")
 
-#Comparar resulados
-if  OP_JUGADDOR == OP_COMP:
-    print ("Empate")
-elif (OP_JUGADDOR == 1 & OP_COMP == 3) or (OP_JUGADDOR == 2 & OP_COMP == 1) or (OP_JUGADDOR == 3 & OP_COMP == 2):
-    print ("Ganaste")
-elif (OP_JUGADDOR == 1 & OP_COMP == 2) or (OP_JUGADDOR == 2 & OP_COMP == 3) or (OP_JUGADDOR == 3 & OP_COMP == 1):
-    print ("Perdiste")    
-else :
-    print ("Hubo un error")
+    #Mostrar eleccion del jugador
+    if  OP_JUGADDOR == "1":
+      print ("Tu elección fue: Piedra")
+    elif OP_JUGADDOR == "2":
+      print ("Tu elección fue: Papel")
+    else:
+      print ("Tu elección fue: Tijera")
+
+    #Mostrar resultado
+    if  OP_JUGADDOR == OP_COMP:
+      print ("\n\t>>> EMPATE <<<")
+    elif (OP_JUGADDOR == "1" and OP_COMP == "3") or (OP_JUGADDOR == "2" and OP_COMP == "1") or (OP_JUGADDOR == "3" and OP_COMP == "2"):
+      print ("\n\t>>> GANASTE <<<")
+    elif (OP_JUGADDOR == "1" and OP_COMP == "2") or (OP_JUGADDOR == "2" and OP_COMP == "3") or (OP_JUGADDOR == "3" and OP_COMP == "1"):
+      print ("\n\t>>> PERDISTE <<<") 
+    
+    #Preguntar si desea continuar con el juego
+    respuesta = input("\n¿Quieres seguir jugando? (s/n): ").lower()
+    if respuesta != "s" and respuesta != "Si" and respuesta != "si":
+        print("Saliendo del programa...")
+        break # Salir del bucle y terminar el programa
+  
+  elif OP_JUGADDOR == "4":
+    print ("Gracias por jugar")
+    break # Salir del bucle y terminar el programa
+  else:
+    print("Opción incorrecta. Por favor, elige una opción ente 1- 3.")
